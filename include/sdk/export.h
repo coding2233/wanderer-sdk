19b3b1b4ff62a1f2f1d99db1a4a7ae745e4f3b92
coding2233 <coding2233@users.noreply.github.com>
2021-06-20 09:22:18 +0000

    [CI-FOR-MAIN] Publish from coding2233/wanderer:main/.

--- include/sdk/export.h
+++ include/sdk/export.h
@@ -13,31 +13,35 @@
 #if WIN32
 #include "windows.h"
 // _declspec(dllexport) _cdecl
+#if BUILD_SDK
 #define WANDERER_EXPORT extern "C" _declspec(dllexport)
+#else
+#define WANDERER_EXPORT extern "C" _declspec(dllimport)
+#endif
 #elif __unix__ || __APPLE__
 #define WANDERER_EXPORT extern "C"
 #endif
 
-    typedef void (*RECEIVE_FUNC)(int, const char *, int);
-    typedef void (*CONNECT_CALLBACK_CFUNC)(bool,const char *);
-    typedef void (*LOGIN_CALLBACK_CFUNC)(bool,const char *);
-    // typedef void (*LOGIN_CALLBACK)(bool, const char *);
+typedef void (*RECEIVE_FUNC)(int, const char *, int);
+typedef void (*CONNECT_CALLBACK_CFUNC)(bool, const char *);
+typedef void (*LOGIN_CALLBACK_CFUNC)(bool, const char *);
+// typedef void (*LOGIN_CALLBACK)(bool, const char *);
 
-    WANDERER_EXPORT void Test(const char *data);
+WANDERER_EXPORT void Test(const char *data);
 
-    WANDERER_EXPORT void Connect(const char *server_ip, int server_port,CONNECT_CALLBACK_CFUNC connect_callback);
+WANDERER_EXPORT void Connect(const char *server_ip, int server_port, CONNECT_CALLBACK_CFUNC connect_callback);
 
-    void ConnectCallback(bool result,std::string messsage);
+void ConnectCallback(bool result, std::string messsage);
 
-    WANDERER_EXPORT void DisConnect();
+WANDERER_EXPORT void DisConnect();
 
-    WANDERER_EXPORT void Update();
+WANDERER_EXPORT void Update();
 
-    WANDERER_EXPORT void Send(int fd, const char *data, size_t size);
+WANDERER_EXPORT void Send(int fd, const char *data, size_t size);
 
-    WANDERER_EXPORT void Login(const char *user_name, const char *password, LOGIN_CALLBACK_CFUNC login_callback);
+WANDERER_EXPORT void Login(const char *user_name, const char *password, LOGIN_CALLBACK_CFUNC login_callback);
 
-    void LoginCallback(bool result,std::string messsage);
+void LoginCallback(bool result, std::string messsage);
 
 // #ifdef __cplusplus
 // }
